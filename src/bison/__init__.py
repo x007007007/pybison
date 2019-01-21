bddf71924f90b9ce30e5beb09afed5eeeb86fd72
Sebastian Brodehl <brodehl@uni-mainz.de>
2019-01-21 10:28:16 +0100

    Quick fix: Compile things to a common place.

--- src/bison/__init__.py
+++ src/bison/__init__.py
@@ -156,7 +156,7 @@ class BisonParser(object):
         """
         self.debug = kw.get('debug', 0)
 
-        self.buildDirectory = 'pybison_' + type(self).__name__ + os.path.sep
+        self.buildDirectory = '/tmp/pybison/pybison_' + type(self).__name__ + os.path.sep
         if self.debug:
             shutil.rmtree(self.buildDirectory, ignore_errors=True)
         makedirs(self.buildDirectory, exist_ok=True)

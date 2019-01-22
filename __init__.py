3194a18477a6f2fa7d99e4d5154ae4e0e15ad0d7
Sebastian Brodehl <brodehl@uni-mainz.de>
2019-01-22 13:53:54 +0100

    Remove file.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .src.python.node import BisonNode
-from .src.python.bison import BisonParser, BisonSyntaxError
-
-__all__ = ['BisonNode', 'BisonParser', 'BisonSyntaxError']

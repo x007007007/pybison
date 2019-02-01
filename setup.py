26a8db3383554e1d728095e6b38938f4e8ad754d
David Hartmann <dhartmann@uni-mainz.de>
2019-02-01 16:51:42 +0100

    bugfix: not installable without cython

--- setup.py
+++ setup.py
@@ -9,16 +9,62 @@ import os
 from os.path import basename, join, splitext
 from glob import glob
 from setuptools import find_packages, setup
-from Cython.Distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Compiler.Options import get_directive_defaults
+
+# source: https://github.com/Blosc/bcolz/blob/master/setup.py#L34-L78
+class LazyCommandClass(dict):
+    """
+    Lazy command class that defers operations requiring Cython and numpy until
+    they've actually been downloaded and installed by setup_requires.
+    """
+    def __contains__(self, key):
+        return (
+            key == 'build_ext'
+            or super(LazyCommandClass, self).__contains__(key)
+        )
+
+    def __setitem__(self, key, value):
+        if key == 'build_ext':
+            raise AssertionError("build_ext overridden!")
+        super(LazyCommandClass, self).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        if key != 'build_ext':
+            return super(LazyCommandClass, self).__getitem__(key)
+
+        from Cython.Distutils import build_ext as cython_build_ext
+        from Cython.Compiler.Options import get_directive_defaults
+        get_directive_defaults()['linetrace'] = True
+        get_directive_defaults()['binding'] = True
+
+        class build_ext(cython_build_ext):
+            """
+            Custom build_ext command that lazily adds numpy's include_dir to
+            extensions.
+            """
+            def build_extensions(self):
+                """
+                Lazily append numpy's include directory to Extension includes.
+                This is done here rather than at module scope because setup.py
+                may be run before numpy has been installed, in which case
+                importing numpy and calling `numpy.get_include()` will fail.
+                """
+                # numpy_incl = resource_filename('numpy', 'core/include')
+                # for ext in self.extensions:
+                #     ext.include_dirs.append(numpy_incl)
+
+                # This explicitly calls the superclass method rather than the
+                # usual super() invocation because distutils' build_class, of
+                # which Cython's build_ext is a subclass, is an old-style class
+                # in Python 2, which doesn't support `super`.
+                cython_build_ext.build_extensions(self)
+        return build_ext
+
+from setuptools import Extension
 
 version = '0.2'
 
 package_data = []
 
-get_directive_defaults()['linetrace'] = True
-get_directive_defaults()['binding'] = True
 
 if sys.platform == 'win32':
     libs = []
@@ -73,7 +119,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    cmdclass={'build_ext': build_ext},
+    cmdclass=LazyCommandClass(),
     scripts=[bison2pyscript],
     install_requires=[
         "cython",

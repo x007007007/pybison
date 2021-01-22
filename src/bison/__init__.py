18d6a28254ae2535f98091431b97b06dc0cb8e14
Sebastian Brodehl <foss@s14l.de>
2021-01-22 16:06:32 +0100

    Release 0.2.9

--- src/bison/__init__.py
+++ src/bison/__init__.py
@@ -42,7 +42,7 @@ WIN_FLEX = join( WIN_CHOCO_DIR, 'win_flex.exe')
 WIN_BISON = join( WIN_CHOCO_DIR, 'win_bison.exe')
 
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 __uri__ = 'https://github.com/lukeparser/pybison'
 __author__ = 'David McNab'
 __maintainer__ = 'Lukeparser Team'
